b6fd916e9d1149622c0c1fbbaac458f22e99d652
Johannes Hengstler <ubezl@student.kit.edu>
2023-11-30 16:59:48 +0100

    remove example code

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,3 @@ mod samples;
 mod ancestors;
 
 mod dna;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
