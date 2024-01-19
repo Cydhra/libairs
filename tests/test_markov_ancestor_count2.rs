11f76a2f8e1779c90082980c78db8a505687bd14
Johannes Hengstler <ubezl@student.kit.edu>
2024-01-19 17:35:49 +0100

    fix: counted the number of ancestors wrong during the viterbi algorithm

---
+++ tests/test_markov_ancestor_count2.rs
@@ -0,0 +1,43 @@
+//! tests for a regression where airs would not correctly count the number of ancestors during the viterbi algorithm.
+//! It used the amount of active ancestor at any given site, but the Markov Chain requires it to use the full amount
+//! of ancestors theoretically available, even if the ancestors state is unknown at that site.
+
+use libairs::ancestors::AncestorGenerator;
+use libairs::dna::{SequencePosition, VariantSite};
+use libairs::ts::TreeSequenceGenerator;
+
+#[test]
+fn test_markov_ancestor_count2() {
+    let sites = [
+        [0, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0, 0, 1, 1],
+        [0, 0, 1, 1, 0, 0, 1, 0],
+        [1, 0, 1, 1, 1, 0, 1, 0],
+        [1, 0, 1, 1, 1, 0, 1, 0],
+        [1, 1, 0, 0, 1, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1, 0, 1],
+    ];
+
+    let ag = AncestorGenerator::from_iter(
+        sites
+            .iter()
+            .enumerate()
+            .map(|(i, site)| VariantSite::new(site.to_vec(), i + 1)),
+    );
+
+    let ancestors = ag.generate_ancestors();
+    let ancestor_matcher = TreeSequenceGenerator::new(
+        ancestors,
+        SequencePosition::from_usize(9),
+        1e-2,
+        1e-20,
+        SequencePosition::from_vec(vec![1, 2, 3, 4, 5, 6, 7, 8]),
+    );
+    let ts = ancestor_matcher.generate_tree_sequence().0;
+
+    // when the ancestor count is incorrect, the algorithm will recombine the last ancestor too early, which will
+    // increase the number of trees in the sequence
+
+    assert_eq!(ts[7].node_intervals[0].end, SequencePosition::from_usize(4));
+}
