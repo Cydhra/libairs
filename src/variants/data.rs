e7206532eb010120bd828a00315da8ef151d5b3d
Johannes Hengstler <ubezl@student.kit.edu>
2024-03-07 15:23:12 +0100

    split variant module into multiple submodules

---
+++ src/variants/data.rs
@@ -0,0 +1,4 @@
+/// Holds variant data and associated metadata.
+/// This data is used as input for both ancestor generation and matching sample data against the
+/// partial tree sequence.
+pub struct VariantData {}
