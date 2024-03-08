6080489fbbc2f1c91f7ffbb1a38047629ca7d29a
Johannes Hengstler <ubezl@student.kit.edu>
2024-03-08 14:58:22 +0100

    added check to builder for adding variants in increasing order

--- src/variants/builder.rs
+++ src/variants/builder.rs
@@ -7,6 +7,7 @@ pub struct VariantDataBuilder {
     sites: Vec<VariantSite>,
     positions: Vec<SequencePosition>,
     num_samples: usize,
+    last_position: Option<SequencePosition>,
     sequence_length: SequencePosition,
 }
 
@@ -17,6 +18,7 @@ impl VariantDataBuilder {
             sites: Vec::default(),
             positions: Vec::default(),
             num_samples: 0,
+            last_position: None,
             sequence_length: SequencePosition::from_usize(sequence_length),
         }
     }
@@ -25,6 +27,7 @@ impl VariantDataBuilder {
     /// (for example, if it is a singleton mutation), it will be ignored.
     /// This behavior will likely change in the future, so the algorithm can at least encode the
     /// additional information into the final tree sequence, even if not used for inference.
+    /// Mutation sites must be added in increasing order of sequence position.
     ///
     /// # Parameters
     /// - `state` a vector of [`MutationState`] values that indicate the variant call for each
@@ -35,6 +38,7 @@ impl VariantDataBuilder {
     /// # Panics
     /// - if the state vector is empty
     /// - if the state vector has a different number of entries than previously added state vectors
+    /// - if a position is added that is not greater than the last position added
     pub fn add_variant_site(&mut self, state: Vec<MutationState>, sequence_position: usize) {
         // TODO store invalid variant sites somewhere for encoding in final tree sequence
         assert!(!state.is_empty());
@@ -45,6 +49,13 @@ impl VariantDataBuilder {
         }
 
         let sequence_position = SequencePosition::from_usize(sequence_position);
+
+        if self.last_position < Some(sequence_position) {
+            self.last_position = Some(sequence_position);
+        } else {
+            panic!("Variant sites must be added in increasing order of sequence position");
+        }
+
         let variant_site = VariantSite::new(state, sequence_position);
         if Self::is_valid_site(&variant_site) {
             self.positions.push(sequence_position);
