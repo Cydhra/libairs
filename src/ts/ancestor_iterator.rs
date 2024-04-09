571a79bd3507d81d7f121fb76b4b4d1713eff905
Johannes Hengstler <ubezl@student.kit.edu>
2024-04-09 18:04:56 +0200

    fixed subtrees not properly updated on site 0

--- src/ts/ancestor_iterator.rs
+++ src/ts/ancestor_iterator.rs
@@ -1038,12 +1038,14 @@ impl<'o> MarginalTree<'o> {
             if self.uncompressed_parents[node.0] == old_parent {
                 self.uncompressed_parents[node.0] = new_parent;
 
-                if self.is_compressed(node) && site_index > 0 {
-                    // record event for traceback that starting from here we are compressed into the old parent
-                    self.viterbi_events[node.0].push(ViterbiEvent {
-                        kind: ViterbiEventKind::Copy(old_parent.unwrap()),
-                        site: self.start + site_index - 1,
-                    });
+                if self.is_compressed(node) {
+                    if site_index > 0 {
+                        // record event for traceback that starting from here we are compressed into the old parent
+                        self.viterbi_events[node.0].push(ViterbiEvent {
+                            kind: ViterbiEventKind::Copy(old_parent.unwrap()),
+                            site: self.start + site_index - 1,
+                        });
+                    }
 
                     queue.extend(self.children[node.0].iter().copied());
                 }
